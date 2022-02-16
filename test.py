283ebc255eda3b0a42b858cdb67bdbbdfd76339f
kunidesuyo <kunidesuyo1234@gmail.com>
2022-02-17 05:10:00 +0900

    検証

--- test.py
+++ test.py
@@ -1,12 +1,6 @@
 from xiangting.xiangting import calculate_number_of_xiangting
 
 
-
-
-# 13か14牌
-
-
-
 mytiles = []
 a = []
 b = []
@@ -21,15 +15,26 @@ mytiles.append(b.copy())
 
 tiles = input()
 
+if len(tiles) != 13*2 and len(tiles) != 14*2:
+    print("invalid input")
+    exit()
+
 ch = {'m': 0, 'p': 1, 's': 2, 'z': 3}
 
 for i in range(len(tiles)//2):
     num = int(tiles[2*i]) - 1
     c = ch[tiles[2*i+1]]
     mytiles[c][num] += 1
-    
 
 print(mytiles)
 xiangting = calculate_number_of_xiangting(mytiles)
 for key, value in xiangting.items():
     print(key, value)
+
+
+
+t = 1
+for _ in range(14):
+    t *= 2
+t *= 10*6*2
+print(t)
