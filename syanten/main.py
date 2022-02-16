2df4e7fcd0f547af3215b68b402680543a3c00f3
kunidesuyo <kunidesuyo1234@gmail.com>
2022-02-16 12:14:03 +0900

    説明追加

--- syanten/main.py
+++ syanten/main.py
@@ -18,22 +18,23 @@
 #     (2)七対子
 #     (3)国士無双
 #     '''
-#     r = mentsu(mytiles)
+#     r = (mytiles)
     
 
-# def mentsu(mytiles):
-#     n_mentu = 0
-#     n_tatsu = 0
+# def four_mentsu_one_head(mytiles):
 #     for i in range(3):
 #         # 数牌
 #         n_mentu, n_tatsu += dfs(mytiles[i])
 
-def score(mt):
-    return 8 - 2*mt[0] - mt[1]
+# def score(mt):
+#     return 8 - 2*mt[0] - mt[1]
 
 memo = {}
 
 def dfs(tiles):
+    '''
+    数牌(萬子索子筒子)に対する面子とターツの数を返す関数
+    '''
     # メモ化再帰
     # 辞書型のkeyにlist型は設定できない
     # if tiles in memo:
@@ -93,17 +94,9 @@ def dfs(tiles):
             now_mt = dfs(next_tiles).copy()
             mts.append(now_mt)
 
-    # ret_mt = [n_mentsu, n_tatsu]
-    # min_shanten = score((n_mentsu, n_tatsu))
-    #print("tiles: ", tiles)
-    # if mts == []:
-    #     memo[key_of_tiles] = [0, 0]
-    #     return memo[key_of_tiles]
     min_shanten = 8
     ret_mt = [0, 0]
-    # print(tiles)
     for mt in mts:
-        # print(mt)
         now_shanten = score(mt)
         if min_shanten > now_shanten:
             ret_mt = mt.copy()
@@ -112,12 +105,7 @@ def dfs(tiles):
     memo[key_of_tiles] = ret_mt.copy()
     return memo[key_of_tiles]
 
-# print("test")
-# a = []
-# b = 1
-# c = 2
-# a.append((b, c))
-# print(a)
+
 
 
 
