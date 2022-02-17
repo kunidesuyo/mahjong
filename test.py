01d250448a45dd6973712a7725e11e6c9c4a0ad4
kunidesuyo <kunidesuyo1234@gmail.com>
2022-02-17 21:55:39 +0900

    微修正

--- test.py
+++ test.py
@@ -2,32 +2,29 @@ from xiangting.xiangting import calculate_number_of_xiangting
 
 
 mytiles = []
-a = []
-b = []
-for i in range(9):
-    a.append(0)
-for i in range(7):
-    b.append(0)
+a = [0 for i in range(9)]
+b = [0 for i in range(7)]
 for i in range(3):
     mytiles.append(a.copy())
 mytiles.append(b.copy())
 
 
-tiles = input()
+with open('hand.txt', 'r') as f:
+    input_tiles = f.read()
 
-if len(tiles) != 13*2 and len(tiles) != 14*2:
-    print("invalid input")
-    exit()
+    if len(input_tiles) != 13*2 and len(input_tiles) != 14*2:
+        print("invalid input")
+        exit()
 
-ch = {'m': 0, 'p': 1, 's': 2, 'z': 3}
+    ch = {'m': 0, 'p': 1, 's': 2, 'z': 3}
 
-for i in range(len(tiles)//2):
-    num = int(tiles[2*i]) - 1
-    c = ch[tiles[2*i+1]]
-    mytiles[c][num] += 1
+    for i in range(len(input_tiles)//2):
+        num = int(input_tiles[2*i]) - 1
+        c = ch[input_tiles[2*i+1]]
+        mytiles[c][num] += 1
 
-print(mytiles)
-xiangting = calculate_number_of_xiangting(mytiles)
-for key, value in xiangting.items():
-    print(key, value)
+    print(mytiles)
+    xiangting = calculate_number_of_xiangting(mytiles)
+    for key, value in xiangting.items():
+        print(key, value)
 
